df0ecc69cbc5790a83945d824b4c408f8ecf2091
Levi Fuller <Levi Fuller>
2016-05-29 23:08:42 -0600

    Initial Commit

---
+++ app/PAGES/login.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    moduleId: 'app/PAGES/',
+    selector: 'login-page',
+    templateUrl: 'login.html'
+})
+export class LoginPage {
+    hello: string = "Kill Them All";
+}
---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: 'app/',
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+})
+export class AppComponent { 
+    
+}
